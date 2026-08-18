9fcbe1c6f7b5ba4ec86d3e7b6e4b4ddaf5faa1df
シロ <shigeyama921@gmail.com>
2026-08-18 20:47:38 +0900

    fix: use briefing category for interview commands

--- test/evaluationConfig.test.js
+++ test/evaluationConfig.test.js
@@ -2,7 +2,9 @@ const test = require("node:test");
 const assert = require("node:assert/strict");
 
 const { EvaluationService } = require("../dist/service/evaluationService.js");
+const { InterviewService } = require("../dist/service/interviewService.js");
 const { BASE_EVALUATION_DAYS } = require("../dist/constant/evaluation.js");
+const { CATEGORY_IDS } = require("../dist/constant/id.js");
 const {
   EVALUATION_SHEET_MESSAGES,
 } = require("../dist/constant/evaluationSheet.js");
@@ -16,6 +18,18 @@ test("評価シートの対象ロール名は旅人", () => {
   assert.match(EVALUATION_SHEET_MESSAGES.NO_KARIMEN_ROLE, /旅人/);
   assert.doesNotMatch(EVALUATION_SHEET_MESSAGES.NO_TARGET_USERS, /未契約/);
   assert.doesNotMatch(EVALUATION_SHEET_MESSAGES.NO_KARIMEN_ROLE, /未契約/);
+});
+
+test("面接通過と評価シートは説明会カテゴリで実行する", () => {
+  const interaction = { channel: { parentId: "1534638743619637465" } };
+
+  assert.equal(CATEGORY_IDS.INTERVIEW, "1534638743619637465");
+  assert.doesNotThrow(() =>
+    InterviewService.validateCommandCategory(interaction),
+  );
+  assert.doesNotThrow(() =>
+    EvaluationService.validateCommandCategory(interaction),
+  );
 });
 
 test("評価シートは指定された4つのフォーラムに作成する", () => {
